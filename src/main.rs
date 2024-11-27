398277ef447bca833437e5320042343cae4ea995
Kartonowy <ttrollface.com.pl@gmail.com>
2024-11-27 13:59:39 +0100

    some writing

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,16 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
-static HI: &[u8] = b"Hiii!!";
-
 #[no_mangle]
 pub unsafe extern "C" fn _start() -> ! {
-    let vga = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HI.iter().enumerate() {
-        unsafe {
-            *vga.offset(i as isize * 2) = byte;
-            *vga.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    panic!("im on fire");
     loop {}
 }
